7dfc70849093154996629f645e911167d8200a14
Abhishek-1839 <abhishektomarsingh7@gmail.com>
2024-09-16 12:48:26 +0530

    middleware

--- routes/eventRouter.js
+++ routes/eventRouter.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const EventController = require('../controllers/eventcontroller');
-const authMiddleware = require('./middleware/auth');
+const authMiddleware = require('../middleware/auth');
 const upload = require('../middleware/upload');
 
 
