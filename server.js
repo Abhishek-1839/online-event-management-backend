856ef1a95eca0f9a9f9623534e470820b2d11f4d
Abhishek-1839 <abhishektomarsingh7@gmail.com>
2024-09-23 20:37:19 +0530

    fgfsd

--- server.js
+++ server.js
@@ -31,8 +31,9 @@ app.use('/', webhookRouter);
 // CORS configuration options
 const corsOptions = {
     // Option 1: Allow all origins (least secure, most permissive)
-    origin: 'https://fantastic-twilight-322d07.netlify.app',
-    
+    // origin: 'https://fantastic-twilight-322d07.netlify.app',
+    // origin: 'http://localhost:5173',
+    origin: 'https://ubiquitous-halva-cf131d.netlify.app',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
